37b58d6c7bc99557144c6b27997b1a8a473fbb69
Lautaro Villarreal Culic <lautaroculic1@gmail.com>
2021-05-27 04:16:03 -0300

    Add files via upload

---
+++ zPassword-Linux.py
@@ -0,0 +1,14 @@
+from random import sample
+def password_generator(longitud):
+	abc_minusculas = "abcdefghijkmnlopqrstuvwxyz"
+	abc_mayusculas = abc_minusculas.upper()
+	numeros = "0123456789"
+	simbolos = "@#$_&-+()/*'|={}\:;][><!?"
+	combinacion = abc_minusculas + abc_mayusculas + numeros + simbolos
+	password_union = sample(combinacion, longitud)
+	password_result = "".join(password_union)
+	return password_result
+password = password_generator(12)
+print("zPassword - Lautaro Villarreal Culic'")
+print("Contraseña: ", password)
+print("lautarovculic.com")
